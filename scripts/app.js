3114572f59448b647d1aa406e4326496da2b8e98
Beata <beata@jasinski.com>
2024-02-22 21:23:24 -0800

    string interpretation, events

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,14 @@ const TodosApp = {
         return {
             newTodo: 'Learn Vue.js'
         };
+    },
+    methods: {
+        saveTodo(event) {
+           event.preventDefault();
+           this.newTodo = 'Updated!' 
+        }
     }
+
 };
 
 Vue.createApp(TodosApp).mount('#todos-app');
