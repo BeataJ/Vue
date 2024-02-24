c2a88791a81d5c1cff6a57f2f28c8155d8204848
Beata <beata@jasinski.com>
2024-02-24 00:56:07 -0800

    delete update save add

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,55 @@
 const TodosApp = {
     data() {
         return {
-            newTodo: 'Learn Vue.js'
+            todos: [],
+            enteredTodoText: '',
+            editedTodoId: null
         };
     },
     methods: {
         saveTodo(event) {
            event.preventDefault();
-           this.newTodo = 'Updated!' 
+
+           if(this.editedTodoId) {
+            //updating
+                const todoId = this.editedTodoId
+                const todoIndex = this.todos.findIndex((todoItem) => {
+                    return todoItem.id === todoId
+                });
+
+                const updatedTodoItem = {
+                    id: this.todos[todoIndex].id,
+                    text: this.enteredTodoText
+                };
+
+                this.todos[todoIndex] = updatedTodoItem;
+                this.editedTodoId = null;
+
+           } else {
+            //creating
+               const newTodo = {
+                text: this.enteredTodoText,
+                id: new Date().toISOString()
+               };
+               this.todos.push(newTodo);
+           }
+
+
+           this.enteredTodoText = ''
+        },
+        startEditTodo(todoId) {
+            this.editedTodoId = todoId;
+            const todo = this.todos.find((todoItem) => {
+                return todoItem.id === todoId;
+            })
+            this.enteredTodoText = todo.text
+        },
+        deleteTodo(todoId) {
+            this.todos = this.todos.filter((todoItem) => {
+                return todoItem.id !== todoId;
+            })
         }
     }
-
 };
 
 Vue.createApp(TodosApp).mount('#todos-app');
