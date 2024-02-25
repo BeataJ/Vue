2d22ecad5473c47abf7c37b5037315d24a66d908
Beata <beata@jasinski.com>
2024-02-25 00:00:42 -0800

    patch delete

--- scripts/app.js
+++ scripts/app.js
@@ -1,55 +1,140 @@
 const TodosApp = {
-    data() {
-        return {
-            todos: [],
-            enteredTodoText: '',
-            editedTodoId: null
-        };
-    },
-    methods: {
-        saveTodo(event) {
-           event.preventDefault();
+  data() {
+    return {
+      isLoading: false,
+      todos: [],
+      enteredTodoText: "",
+      editedTodoId: null,
+    };
+  },
+  methods: {
+    async saveTodo(event) {
+      event.preventDefault();
 
-           if(this.editedTodoId) {
-            //updating
-                const todoId = this.editedTodoId
-                const todoIndex = this.todos.findIndex((todoItem) => {
-                    return todoItem.id === todoId
-                });
+      if (this.editedTodoId) {
+        //updating
+        const todoId = this.editedTodoId;
 
-                const updatedTodoItem = {
-                    id: this.todos[todoIndex].id,
-                    text: this.enteredTodoText
-                };
+        const todoIndex = this.todos.findIndex((todoItem) => {
+            return todoItem.id === todoId;
+          });
 
-                this.todos[todoIndex] = updatedTodoItem;
-                this.editedTodoId = null;
+          
+        const updatedTodoItem = {
+            id: this.todos[todoIndex].id,
+            text: this.enteredTodoText,
+          };
+  
+          this.todos[todoIndex] = updatedTodoItem;
+          this.editedTodoId = null;
 
-           } else {
-            //creating
-               const newTodo = {
-                text: this.enteredTodoText,
-                id: new Date().toISOString()
-               };
-               this.todos.push(newTodo);
-           }
+        let response;
 
-
-           this.enteredTodoText = ''
-        },
-        startEditTodo(todoId) {
-            this.editedTodoId = todoId;
-            const todo = this.todos.find((todoItem) => {
-                return todoItem.id === todoId;
-            })
-            this.enteredTodoText = todo.text
-        },
-        deleteTodo(todoId) {
-            this.todos = this.todos.filter((todoItem) => {
-                return todoItem.id !== todoId;
-            })
+        try {
+          response = await fetch("http://localhost:3000/todos/" + todoId, {
+            method: "PATCH",
+            body: JSON.stringify({
+              newText: this.enteredTodoText,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
         }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+
+      } else {
+        //creating
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              text: this.enteredTodoText,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        const responseData = await response.json();
+        const newTodo = {
+          text: this.enteredTodoText,
+          id: responseData.createdTodo.id,
+        };
+
+        this.todos.push(newTodo);
+      }
+
+      this.enteredTodoText = "";
+    },
+    startEditTodo(todoId) {
+      this.editedTodoId = todoId;
+      const todo = this.todos.find((todoItem) => {
+        return todoItem.id === todoId;
+      });
+      this.enteredTodoText = todo.text;
+    },
+     async deleteTodo(todoId) {
+      this.todos = this.todos.filter( (todoItem) => {
+        return todoItem.id !== todoId;
+        });
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3000/todos/" + todoId, {
+            method: "DELETE",
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+      
+    },
+  },
+  async created() {
+    let response;
+    this.isLoading = true;
+    try {
+      response = await fetch("http://localhost:3000/todos");
+    } catch (error) {
+      alert("Something went wrong!");
+      this.isLoading = false;
+      return;
     }
+
+    this.isLoading = false;
+
+    if (!response.ok) {
+      alert("Something went wrong!");
+      return;
+    }
+
+    const responseData = await response.json();
+    this.todos = responseData.todos;
+  },
 };
 
-Vue.createApp(TodosApp).mount('#todos-app');
+Vue.createApp(TodosApp).mount("#todos-app");
